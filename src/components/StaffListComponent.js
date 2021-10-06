b60871e3cc2401538a1bb1860ff0f652e8504b6f
Loc <locphfx11236@funix.edu.vn>
2021-10-06 22:46:27 +0700

    save03

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { STAFFS } from '../shared/staffs.jsx';
+import dateFormat from 'dateformat';
+
+class StaffList extends Component {
+    
+}
+
+export default StaffList;
