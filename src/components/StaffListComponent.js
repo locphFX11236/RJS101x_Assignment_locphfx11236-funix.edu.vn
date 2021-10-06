fe5f8675258255c05a66b82e4834a42ad0c8e819
Loc <locphfx11236@funix.edu.vn>
2021-10-06 22:50:49 +0700

    save07

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -4,15 +4,45 @@ import dateFormat from 'dateformat';
 
 class StaffList extends Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            staffs : STAFFS,
+            selectedList : null
+        };
+    }
+    
+    onListSelect(staff) {
+        this.setState({ selectedList: staff});
+    }
+
+    renderSelectList (staff) {
+        if (staff != null) {
+            
+        } else {
+            return(
+                <div><p>Bấm vào tên nhân viên để xem thông tin</p></div>
+            );
+        }
+    }
+
     render() {
+
+        const List = this.state.staffs.map((staff) => {
+            return (
+               <div key={staff.id}>
+                   {staff.name}
+               </div> 
+            )
+        });
 
         return (
             <div>
                 <div>
-                    {}
+                    {List}
                 </div>
                 <div>
-                    {}
+                    {this.renderSelectList(this.state.selectedList)}
                 </div>
             </div>
         );
