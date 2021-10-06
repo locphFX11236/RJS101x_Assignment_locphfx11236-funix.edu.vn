59cdbe9b5a21048331b48bb49a95c7a9986050fb
Loc <locphfx11236@funix.edu.vn>
2021-10-06 22:45:38 +0700

    save02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import StaffList from './components/StaffListComponent';
 
 
 class App extends Component {
@@ -8,6 +9,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <nav>Danh Sách</nav>
+        <StaffList />
       </div>
     );
   }
