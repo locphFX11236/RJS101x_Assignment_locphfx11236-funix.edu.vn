d137395e7471b0075352a85c5d5f6988893b57d9
Loc <locphfx11236@funix.edu.vn>
2021-10-06 22:44:35 +0700

    save01

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        <StaffLis />
       </div>
     );
   }
