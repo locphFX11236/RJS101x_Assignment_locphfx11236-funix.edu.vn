762b4d55a7c89cf35a79ed99c873f7895b990d58
Loc <locphfx11236@funix.edu.vn>
2021-10-21 21:38:14 +0700

    Tinh Luong

--- src/components/SalaryComponent.js
+++ src/components/SalaryComponent.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+function Luong({salaryScale, overTime}) {
+    const basicSalary = 3000000;
+    const overTimeSalary = 200000;
+    let salary = (parseFloat(salaryScale) * basicSalary) + (parseFloat(overTime) * overTimeSalary) ;
+    return Math.round(salary)
+}
+
 const Salary = (props) => {
     const List = props.staffs.map((staff) => {
         return ( 
@@ -10,7 +17,9 @@ const Salary = (props) => {
                 <p>Mã nhân viên: {staff.id}</p>
                 <p>Hệ số lương: {staff.salaryScale}</p>
                 <p>Số giờ làm thêm: {staff.overTime}</p>
-                <p className="border">Lương: {}</p>
+                <p className="border">Lương: <Luong salaryScale={staff.salaryScale} 
+                 overTime={staff.overTime}
+                /></p>
             </div>
         )
     });
