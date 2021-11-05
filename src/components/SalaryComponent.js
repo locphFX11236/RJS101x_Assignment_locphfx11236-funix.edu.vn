1f961c67b0cf9579d82f9154455b99ef059f9b79
Loc <locphfx11236@funix.edu.vn>
2021-11-05 16:22:11 +0700

    Report Template

--- src/components/SalaryComponent.js
+++ src/components/SalaryComponent.js
@@ -2,24 +2,21 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function Luong({salaryScale, overTime}) {
-    const basicSalary = 3000000;
-    const overTimeSalary = 200000;
-    let salary = (parseFloat(salaryScale) * basicSalary) + (parseFloat(overTime) * overTimeSalary) ;
-    return Math.round(salary)
-}
-
 const Salary = (props) => {
     const List = props.staffs.map((staff) => {
+
+        const basicSalary = 3000000;
+        const overTimeSalary = 200000;
+        let salary = Math.round((parseFloat(staff.salaryScale) * basicSalary) + (parseFloat(staff.overTime) * overTimeSalary)) ;
+        Object.assign(staff, { 'salary': salary });
+
         return ( 
             <div className="border col-12 col-md-5 col-lg-3 m-1">
                 <h3>{staff.name}</h3>
                 <p>Mã nhân viên: {staff.id}</p>
                 <p>Hệ số lương: {staff.salaryScale}</p>
                 <p>Số giờ làm thêm: {staff.overTime}</p>
-                <p className="border">Lương: <Luong salaryScale={staff.salaryScale} 
-                 overTime={staff.overTime}
-                /></p>
+                <p className="border">Lương: {staff.salary}</p>
             </div>
         )
     });
