01dbd1d0bc7c1e77a4c5729635ca519c6e3b9fac
Loc <locphfx11236@funix.edu.vn>
2021-11-07 22:08:34 +0700

    Modal2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -15,11 +15,6 @@ const mapStateToProps = state => {
   }
 
 class Main extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
     render() {
 
         const StaffWithId = ({match}) => {
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText } from 'reactstrap';
 import { Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody,
     Button, Row, Col, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
@@ -12,7 +12,7 @@ function RenderStaffItem (props) {
            <Card key={staff.id} className="border col-6 col-md-4 col-lg-2" >
                 <Link to={`/staff/${staff.id}`} >
                     <CardImg src={staff.image} alt={staff.name} />
-                    <CardTitle>{staff.name}</CardTitle>
+                    <CardText>{staff.name}</CardText>
                 </Link>
            </Card>
         )
@@ -63,20 +63,19 @@ class StaffList extends Component  {
     render() {
         return (
             <div className="container">
-                <div className="row">
+                <Row>
                     <Breadcrumb>
-                        <BreadcrumbItem><Link to="/staff">Staffs List</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Staffs List</BreadcrumbItem>
                     </Breadcrumb>
-                    <Button className="ml-auto" outline onClick={this.toggleModal}> + </Button>
-                </div>
+                </Row>
                 <RenderStaffItem staffs={this.props.staffs} />
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
                     <ModalBody>
                     <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                            <Row className="form-group">
-                                <Label htmlFor="name" md={2}>Nhập tên</Label>
-                                <Col md={10}>
+                            <Row className="form-group m-2">
+                                <Label htmlFor="name" md={5}>Nhập tên</Label>
+                                <Col md={7}>
                                     <Control.text model=".name" name="name"
                                         placeholder="Nhập tên"
                                         className="form-control"
@@ -96,9 +95,9 @@ class StaffList extends Component  {
                                      />
                                 </Col>
                             </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="doB" md={2}>Ngày sinh</Label>
-                                <Col md={10}>
+                            <Row className="form-group m-2">
+                                <Label htmlFor="doB" md={5}>Ngày sinh</Label>
+                                <Col md={7}>
                                     <Control.text model=".doB" name="doB"
                                         placeholder="Ngày sinh"
                                         className="form-control"
@@ -118,9 +117,9 @@ class StaffList extends Component  {
                                      />
                                 </Col>
                             </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="salaryScale" md={2}>Hệ số lương</Label>
-                                <Col md={10}>
+                            <Row className="form-group m-2">
+                                <Label htmlFor="salaryScale" md={5}>Hệ số lương</Label>
+                                <Col md={7}>
                                     <Control.text model=".salaryScale" name="salaryScale"
                                         placeholder="Hệ số lương"
                                         className="form-control"
@@ -141,36 +140,102 @@ class StaffList extends Component  {
                                      />
                                 </Col>
                             </Row>
-                            <Row className="form-group">
-                                <Col md={{size: 6, offset: 2}}>
-                                    <div className="form-check">
-                                        <Label check>
-                                            <Control.checkbox model=".agree" name="agree"
-                                                className="form-check-input" /> {' '}
-                                                <strong>May we contact you?</strong>
-                                        </Label>
-                                    </div>
-                                </Col>
-                                <Col md={{size: 3, offset: 1}}>
-                                    <Control.select model=".contactType" name="contactType"
-                                        className="form-control">
-                                        <option>Tel.</option>
-                                        <option>Email</option>
-                                    </Control.select>
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="message" md={2}>Your Feedback</Label>
-                                <Col md={10}>
-                                    <Control.textarea model=".message" id="message" name="message"
-                                        rows="12"
-                                        className="form-control" />
+                            <Row className="form-group m-2">
+                                <Label htmlFor="startDate" md={5}>Ngày vào công ty</Label>
+                                <Col md={7}>
+                                    <Control.text model=".startDate" name="startDate"
+                                        placeholder="Ngày vào công ty"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                        }}
+                                     />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".startDate"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            minLength: 'Must be greater than 2 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }}
+                                     />
                                 </Col>
                             </Row>
-                            <Row className="form-group">
-                                <Col md={{size:10, offset: 2}}>
+                            <Row className="form-group m-2">
+                                <Label htmlFor="department" md={5}>Phòng ban</Label>
+                                <Col md={7}>
+                                    <Control.text model=".department" name="department"
+                                        placeholder="Phòng ban"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                        }}
+                                     />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".department"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            minLength: 'Must be greater than 2 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group m-2">
+                                <Label htmlFor="annualLeave" md={5}>Số ngày nghỉ còn lại</Label>
+                                <Col md={7}>
+                                    <Control.text model=".annualLeave" name="annualLeave"
+                                        placeholder="Số ngày nghỉ còn lại"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                        }}
+                                     />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".annualLeave"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            minLength: 'Must be greater than 2 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group m-2">
+                                <Label htmlFor="overTime" md={5}>Số ngày đã làm thêm</Label>
+                                <Col md={7}>
+                                    <Control.text model=".overTime" name="overTime"
+                                        placeholder="Số ngày đã làm thêm"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                        }}
+                                     />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".overTime"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required ',
+                                            minLength: 'Must be greater than 2 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }}
+                                     />
+                                </Col>
+                            </Row>
+                            <Row className="form-group m-2">
+                                <Col md={{size:8, offset: 4}}>
                                     <Button type="submit" color="primary">
-                                        Them nhan vien
+                                        Thêm nhân viên
                                     </Button>
                                 </Col>
                             </Row>
